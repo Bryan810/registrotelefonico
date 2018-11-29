93180306edcc55de69dc6d0d560c0303a8e37c94
Bryan <basaltos@uce.edu.ec>
2018-11-28 22:46:05 -0500

    Agregada tabla para usuarios

---
+++ src/main/java/com/ec/golden/entidades/usuarios/Roles.java
@@ -0,0 +1,17 @@
+package com.ec.golden.entidades.usuarios;
+
+
+import lombok.Data;
+
+@Data
+public class Roles {
+    private String role;
+
+
+    protected Roles() {
+    }
+
+    public Roles(String role) {
+        this.role = role;
+    }
+}
---
+++ src/main/java/com/ec/golden/entidades/usuarios/Usuario.java
@@ -0,0 +1,28 @@
+package com.ec.golden.entidades.usuarios;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "Usuarios")
+public class Usuario {
+    @Id
+    private String idUsuario;
+    @Indexed(direction = IndexDirection.ASCENDING)
+    private String usuario;
+    private String nombre;
+    private String apellido;
+    private String clave;
+    private Roles rol;
+
+    public Usuario(String usuario, String nombre, String apellido, String clave, Roles rol) {
+        this.usuario = usuario;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.clave = clave;
+        this.rol = rol;
+    }
+}
